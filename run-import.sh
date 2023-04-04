3fc107e98866402b049294c12d462b3f99d96aee
dacruz_j <dacruz_j@etna-alternance.net>
2023-04-04 17:31:53 +0200

    suppression du winpty inutile et qui n'était pas présent au début

--- run-import.sh
+++ run-import.sh
@@ -1,6 +1,6 @@
 echo "RUNNING APP..."
 echo "---------"
-winpty docker-compose exec app php /var/app/src/app.php || echo "Error while running app..."
+docker-compose exec app php /var/app/src/app.php || echo "Error while running app..."
 
 echo "---------"
 echo 'DONE.'
